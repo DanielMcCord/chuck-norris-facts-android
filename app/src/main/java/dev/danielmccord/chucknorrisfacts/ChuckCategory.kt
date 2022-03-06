8d03cc5a40c4f5543f9f0ec772cc51293b6f1df8
Daniel McCord <git@danielmccord.dev>
2022-03-05 18:11:29 -0800

    Create POJOs

---
+++ app/src/main/java/dev/danielmccord/chucknorrisfacts/ChuckCategory.kt
@@ -0,0 +1,5 @@
+package dev.danielmccord.chucknorrisfacts
+
+import androidx.annotation.ColorRes
+
+class ChuckCategory(val name: String, val description: String, @ColorRes val color: Int)
---
+++ app/src/main/java/dev/danielmccord/chucknorrisfacts/ChuckFact.kt
@@ -0,0 +1,3 @@
+package dev.danielmccord.chucknorrisfacts
+
+class ChuckFact(val categories: List<String>, val createdAt: String, val iconUrl: String, val id: String, val updatedAt: String, val url: String, val value: String)
