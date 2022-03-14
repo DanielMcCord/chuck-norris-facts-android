e0a1ce4f724fd3633c99a1ea72d8c18a5e5e9173
Daniel McCord <git@danielmccord.dev>
2022-03-14 15:14:23 -0700

    Add SerializedName annotations

--- app/src/main/java/dev/danielmccord/chucknorrisfacts/ChuckFact.kt
+++ app/src/main/java/dev/danielmccord/chucknorrisfacts/ChuckFact.kt
@@ -1,3 +1,13 @@
 package dev.danielmccord.chucknorrisfacts
 
-class ChuckFact(val categories: List<String>, val createdAt: String, val iconUrl: String, val id: String, val updatedAt: String, val url: String, val value: String)
+import com.google.gson.annotations.SerializedName
+
+class ChuckFact(
+    @field:SerializedName("categories") val categories: List<String>,
+    @field:SerializedName("created_at") val createdAt: String,
+    @field:SerializedName("icon_url") val iconUrl: String,
+    @field:SerializedName("id") val id: String,
+    @field:SerializedName("updated_at") val updatedAt: String,
+    @field:SerializedName("url") val url: String,
+    @field:SerializedName("value") val value: String
+)
