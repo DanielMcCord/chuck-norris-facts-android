c188e2259d3323a2929e4dabc385e56f2d78bddd
Daniel McCord <git@danielmccord.dev>
2022-03-16 14:25:04 -0700

    Add view binding for table of chucktents

--- app/src/main/java/dev/danielmccord/chucknorrisfacts/CategoriesAdapter.kt
+++ app/src/main/java/dev/danielmccord/chucknorrisfacts/CategoriesAdapter.kt
@@ -1,29 +1,34 @@
 package dev.danielmccord.chucknorrisfacts
 
 import android.content.Context
-import androidx.annotation.ColorInt
-import androidx.recyclerview.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import dev.danielmccord.chucknorrisfacts.databinding.TableOfContentListBinding
 
 class CategoriesAdapter(val context: Context, val categories: List<String>): RecyclerView.Adapter<CategoriesAdapter.CategoryViewHolder>() {
     private val TAG: String = FactsAdapter::class.java.simpleName
 
     class CategoryViewHolder(itemView: View): RecyclerView.ViewHolder(itemView), ListItemClickListener {
+        private var binding: TableOfContentListBinding = TableOfContentListBinding.bind(itemView)
+        var categoryName: TextView = binding.categories
+        var categoryDescription: TextView = binding.descriptions
+
         override fun onListItemClick(itemNumber: Int, itemView: View) {
             TODO("Not yet implemented")
         }
 
         fun bind(listIndex: Int) {
-
+            categoryName.text = listIndex.toString()
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
-        TODO("Not yet implemented")
+        val itemView = LayoutInflater.from(context).inflate(R.layout.table_of_content_list, parent, false)
+        return CategoryViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
